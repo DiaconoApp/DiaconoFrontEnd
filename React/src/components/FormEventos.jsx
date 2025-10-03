50fd769be15f9256d4240df201128eb5a2e25605
ChavesCoder <nata.chaves@sptech.school>
2025-10-03 19:47:13 -0300

    feat: ajustando component de Formulario de eventos

--- React/src/components/FormEventos.jsx
+++ React/src/components/FormEventos.jsx
@@ -1,177 +1,184 @@
 import { useState } from "react";
 
-export function FormEventos() {
+export function FormEventos({ initialData = {}, organizer = {}, onSubmit }) {
   const [formData, setFormData] = useState({
-    nome: "",
-    publico: "",
-    escala: "",
-    data: "",
-    inicio: "",
-    fim: "",
-    custo: "",
-    local: "",
-    descricao: "",
+    nome: initialData.nome || "",
+    publico: initialData.publico || "",
+    escala: initialData.escala || "",
+    data: initialData.data || "",
+    inicio: initialData.inicio || "",
+    fim: initialData.fim || "",
+    custo: initialData.custo || "",
+    local: initialData.local || "",
+    descricao: initialData.descricao || "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    setFormData((p) => ({ ...p, [name]: value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Evento salvo:", formData);
-    // aqui você pode integrar com backend
+    if (onSubmit) onSubmit(formData);
+    else console.log("submit:", formData);
   };
 
-    return(
-        <form
-      onSubmit={handleSubmit}
-      className="bg-white shadow rounded-lg p-6 flex flex-col gap-4"
-    >
-      <h2 className="text-2xl font-bold tracking-[-0.3px] text-icf-primary-400">Adicionar Evento</h2>
+  return (
+    <div className="flex w-full">
+      <div className="bg-white rounded-l-lg p-6 w-[57%] col-span-3">
+        <h2 className="text-2xl font-bold tracking-[-0.4px] mb-4">Adicionar Evento</h2>
 
-      {/* Nome */}
-      <div className="flex flex-col gap-2">
-        <label className="text-base text-icf-primary-400">
-          Nome do evento
-        </label>
-        <input
-          type="text"
-          name="nome"
-          value={formData.nome}
-          onChange={handleChange}
-          placeholder="Digite o nome do evento"
-          className="w-full bg-surface-50 border border-icf-primary-100 rounded-md py-3 px-4"
-        />
-      </div>
+          <div className="">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+              <div>
+                <label className="text-base text-icf-primary-400">Nome do evento</label>
+                <input
+                  type="text"
+                  name="nome"
+                  value={formData.nome}
+                  onChange={handleChange}
+                  placeholder="Culto de Jovens"
+                  className="mt-1 w-full text-base placeholder:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                />
+              </div>
 
-      {/* Público-alvo */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
-          Público-alvo
-        </label>
-        <select
-          name="publico"
-          value={formData.publico}
-          onChange={handleChange}
-          className="w-full border border-gray-300 rounded-md p-2"
-        >
-          <option value="">Selecione...</option>
-          <option value="ministerio">Ministério de Jovens</option>
-          <option value="adultos">Adultos</option>
-        </select>
-      </div>
+              <div className="grid grid-cols-1 gap-4">
+                <div>
+                  <label className="text-base text-icf-primary-400">Público-alvo</label>
+                  <select
+                    name="publico"
+                    value={formData.publico}
+                    onChange={handleChange}
+                    className="mt-1 w-full text-base disabled:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                  >
+                    <option value="" disabled >Selecione...</option>
+                    <option value="jovens">Ministério de Jovens</option>
+                    <option value="adultos">Adultos</option>
+                  </select>
+                </div>
 
-      {/* Escala */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
-          Escala
-        </label>
-        <select
-          name="escala"
-          value={formData.escala}
-          onChange={handleChange}
-          className="w-full border border-gray-300 rounded-md p-2"
-        >
-          <option value="">Selecione...</option>
-          <option value="musica">Ministério de Música</option>
-          <option value="midia">Ministério de Mídia</option>
-        </select>
-      </div>
+                <div>
+                  <label className="text-base text-icf-primary-400">Escala</label>
+                  <select
+                    name="escala"
+                    value={formData.escala}
+                    onChange={handleChange}
+                    className="mt-1 w-full text-base placeholder:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                  >
+                    <option value="">Ministérios obrigatórios</option>
+                    <option value="musica">Ministério de Música</option>
+                  </select>
+                </div>
+              </div>
 
-      {/* Data, Início, Fim */}
-      <div className="grid grid-cols-3 gap-4">
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Data</label>
-          <input
-            type="date"
-            name="data"
-            value={formData.data}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-md p-2"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Início</label>
-          <input
-            type="time"
-            name="inicio"
-            value={formData.inicio}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-md p-2"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Fim</label>
-          <input
-            type="time"
-            name="fim"
-            value={formData.fim}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-md p-2"
-          />
-        </div>
-      </div>
+              <div className="flex items-end gap-3 w-full">
+                <div className="w-[35%]">
+                  <label className="text-base text-icf-primary-400">Data</label>
+                  <input
+                    type="date"
+                    name="data"
+                    value={formData.data}
+                    onChange={handleChange}
+                    className="mt-1 w-full text-base placeholder:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                  />
+                </div>
+                <div className="w-[17.5%]">
+                  <label className="text-base text-icf-primary-400">Início</label>
+                  <input
+                    type="time"
+                    name="inicio"
+                    value={formData.inicio}
+                    onChange={handleChange}
+                    className="mt-1 w-full text-base placeholder:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                  />
+                </div>
+                <div className="pb-3">
+                  <span className="text-base text-icf-primary-200">Até</span>
+                </div>
+              <div className="w-[17.5%]">
+                    <label className="text-base text-icf-primary-400">Fim</label>
+                    <input
+                      type="time"
+                      name="fim"
+                      value={formData.fim}
+                      onChange={handleChange}
+                      className="mt-1 w-full text-base placeholder:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                    />
+              </div>
+              <button
+                    type="button"
+                    className="w-[25%] px-4 py-2.5 rounded-md bg-icf-primary-50 flex justify-center items-center gap-2 text-icf-primary-300 text-base cursor-pointer"
+                    onClick={() => setShowRecorrente(true)}
+              >
+                <img src="/public/calendario.png" alt="" className="w-4 h-4"/>
+                    Tornar Recorrente
+              </button>
+              </div>
 
-      {/* Custo */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">Custo</label>
-        <input
-          type="number"
-          name="custo"
-          value={formData.custo}
-          onChange={handleChange}
-          className="w-full border border-gray-300 rounded-md p-2"
-        />
-      </div>
+              <div className="grid grid-cols-2 gap-4 w-[100%]">
+                <div>
+                  <label className="text-base text-icf-primary-400">Custo</label>
+                  <input
+                    type="number"
+                    name="custo"
+                    value={formData.custo}
+                    onChange={handleChange}
+                    placeholder="R$ 0,00"
+                    className="mt-1 w-full text-base placeholder:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                  />
+                </div>
+                <div>
+                  <label className="text-base text-icf-primary-400">Local</label>
+                  <select
+                    name="local"
+                    value={formData.local}
+                    onChange={handleChange}
+                    className="mt-1 w-full text-base placeholder:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                  >
+                    <option value="">Adicionar localização</option>
+                    <option value="igreja">Igreja</option>
+                  </select>
+                </div>
+              </div>
 
-      {/* Local */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">Local</label>
-        <select
-          name="local"
-          value={formData.local}
-          onChange={handleChange}
-          className="w-full border border-gray-300 rounded-md p-2"
-        >
-          <option value="">Adicionar localização</option>
-          <option value="igreja">Igreja</option>
-          <option value="salão">Salão Social</option>
-        </select>
-      </div>
+              <div>
+                <label className="text-base text-icf-primary-400">Descrição</label>
+                <textarea
+                  name="descricao"
+                  value={formData.descricao}
+                  onChange={handleChange}
+                  rows={5}
+                  className="mt-1 h-16 w-full text-base placeholder:text-icf-primary-200 bg-surface-50 border border-icf-primary-200 rounded-lg py-3 px-4"
+                />
+              </div>
 
-      {/* Descrição */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
-          Descrição
-        </label>
-        <textarea
-          name="descricao"
-          value={formData.descricao}
-          onChange={handleChange}
-          className="w-full border border-gray-300 rounded-md p-2"
-        />
+              <div className="flex gap-3 justify-end mt-4">
+                <button type="button" className="px-8 py-3 rounded-lg bg-icf-primary-200 text-surface-50 text-base font-normal cursor-pointer hover:opacity-90">
+                  Cancelar
+                </button>
+                <button type="submit" className="py-3 px-8 rounded-lg bg-icf-primary-400 text-surface-50 text-base font-normal cursor-pointer hover:opacity-90">
+                  Salvar
+                </button>
+              </div>
+            </form>
+          </div>  
       </div>
+      <div className="bg-white border-l rounded-r-lg py-4 px-9 border-icf-primary-100 col-span-1">
+              <h3 className="pt-8 text-base font-bold text-icf-primary-400">Organizador</h3>
 
-      {/* Botões */}
-      <div className="flex gap-4 justify-end">
-        <button
-          type="submit"
-          className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800"
-        >
-          Salvar
-        </button>
-        <button
-          type="button"
-          className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
-        >
-          Cancelar
-        </button>
-      </div>
-    </form>
-    );
+              <div className="flex items-center gap-1 mt-3">
+                <img
+                  src={organizer.avatar || "/public/calendario.png"}
+                  alt={organizer.name || "Avatar organizador"}
+                  className="w-7 h-7 border border-icf-primary-300 rounded-full object-cover"
+                />
+                <div>
+                  <div className="text-xs font-semibold text-icf-primary-400">{organizer.name || "Samuel Nicolau"}</div>
+                  <div className="text-[10px] tracking-[0.5px] font-light text-icf-primary-300">{organizer.role || "Organizador"}</div>
+                </div>
+              </div>
+            </div>
+    </div>
+  );
 }
